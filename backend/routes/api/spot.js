158d12b0d273a84c6697361f5e69926c2a38d234
FenneAustin <austindfenne@gmail.com>
2022-07-06 16:36:58 -0700

    part of get spotdetails

--- backend/routes/api/spot.js
+++ backend/routes/api/spot.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 const { requireAuth } = require('../../utils/auth');
-const { Spot, Image  } = require('../../db/models');
+const { Spot, Image, Review  } = require('../../db/models');
 
 router.get("/", async (req, res) => {
     const spots = await Spot.findAll({
@@ -21,12 +21,36 @@ router.post('/', requireAuth, async (req,res) => {
   // TODO does this have to be in a JWT? or can it be in the req body
   try{
     const newSpot = await Spot.create({
-      
+
     })
   }
   catch{
 
   }
+})
+
+router.get('/:id', async (req, res) => {
+
+  const {id} = req.params.id;
+  try {
+  const spot = await Spot.findByPk(req.params.id);
+  } catch {
+    return res.status(404).json({ error: "Not found" });
+  }
+  const numReviews = await Review.count({
+    where: {
+      userId: id
+    }
+  })
+
+  const avgStarRating = await Review.avg(stars, {
+    where: {
+      userId: id
+    }
+  })
+
+  return res.json(spot, numReviews,avgStarRating);
+
 })
 
 
