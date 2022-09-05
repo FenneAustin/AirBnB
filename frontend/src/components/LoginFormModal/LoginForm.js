8fb9151d00add8cff5a9f4dea2cd0d042b7d0832
FenneAustin <austindfenne@gmail.com>
2022-09-04 20:08:34 -0700

    Added styling

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -1,24 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import * as sessionActions from "../../store/session";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import './LoginForm.css'
 
-function LoginForm() {
+function LoginForm({closeModal}) {
   const dispatch = useDispatch();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState([]);
+  const [invalidCredentials, setInvalidCredentials] = useState(null)
+
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setErrors([]);
-    return dispatch(sessionActions.login({ email, password })).catch(
+    setInvalidCredentials(null);
+
+    dispatch(sessionActions.login({ email, password })).catch(
       async (res) => {
         const data = await res.json();
-        if (data && data.errors) setErrors(data.errors);
+        if (data && data.errors) setErrors(data.errors)
+        if (data && data.message === 'invalid credentials') {setInvalidCredentials(true)}
+        else if (data.message !== 'invalid credentials') setInvalidCredentials(false)
       }
     );
+
+    if(errors.length < 1 && invalidCredentials === false ) closeModal();
+
   };
+
 
   return (
     <form onSubmit={handleSubmit} className="login-form">
@@ -26,26 +37,33 @@ function LoginForm() {
         {errors.map((error, idx) => (
           <li key={idx}>{error}</li>
         ))}
+        {invalidCredentials ? <li>Invalid crednetials</li> : null}
       </ul>
-      <label>
-        Email
+
+        <lable className="login-title">
+          login
+        </lable>
         <input
           type="text"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
           required
+          className="username-input"
         />
-      </label>
-      <label>
-        Password
+
+
+
         <input
           type="password"
+          placeholder="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
+          className="password-input-field"
         />
-      </label>
-      <button type="submit">Log In</button>
+
+      <button type="submit" className='login-btn'>Log In</button>
     </form>
   );
 }
