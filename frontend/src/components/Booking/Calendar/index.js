488ed43335091e65e74a1b72ea867a5a001b07dc
FenneAustin <austindfenne@gmail.com>
2023-02-15 16:59:45 -0800

    fixing render.com

--- frontend/src/components/Booking/Calendar/index.js
+++ frontend/src/components/Booking/Calendar/index.js
@@ -1,19 +1,12 @@
-import React from "react";
-import {DayPickerRangeController} from "react-dates";
-import 'react-dates/initialize';
 
 
-const Calendar = () => {
-  return (
-    <DayPickerRangeController
-    startDate={this.state.startDate}
-    endDate={this.state.endDate}
-    onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })}
-    focusedInput={this.state.focusedInput}
-    onFocusChange={focusedInput => this.setState({ focusedInput })}
-    initialVisibleMonth={() => moment().add(2, "month")}
-   />
-  );
-};
 
-export default Calendar;
+const Calendar = () =>{
+    return (
+        <div>
+            <h1>Calendar</h1>
+        </div>
+    )
+}
+
+export default Calendar
